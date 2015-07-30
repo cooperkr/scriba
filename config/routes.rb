7a967d42f5bc0dd5ac87d6a8d41573db430baaaa
Krystal Cooper <kcooper4@illinois.edu>
2015-07-30 10:36:02 -0400

    added credit and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
   resources :transcriptions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'transcriptions#index'
+  root 'pages#credit'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
